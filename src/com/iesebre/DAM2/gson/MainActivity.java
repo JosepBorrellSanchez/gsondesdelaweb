272d1eaceae38ee453381e5e9ce4485c30f4a18c
josepborrellsanchez <josepborrellsanchez@iesebre.com>
2014-02-21 18:34:02 +0100

    funciona

---
+++ src/com/iesebre/DAM2/gson/MainActivity.java
@@ -0,0 +1,98 @@
+package com.iesebre.DAM2.gson;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends Activity {
+	private Person person = new Person(1,"hola","asd","asf");
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		
+				//serialization (java to JSON)...
+			//Gson gson = new Gson();
+			//dades contains; Array of UserProfile
+			// Type collectionType = new TypeToken<Person>(){}.getType();
+			//String json = gson.toJson(person);
+			//Log.i("text", json);
+			//on this, parsing java to json
+	                //profiles = gson.fromJson(json, UserProfile[].class);
+			
+			Button btnBoton1 = (Button)findViewById(R.id.button1);
+			
+			 
+			btnBoton1.setOnClickListener(new View.OnClickListener() {
+			    public void onClick(View arg0)
+			    {
+			    	 String missatge = null;
+					 Log.d(missatge,"Botón 1 pulsado!");
+					 DownloadTask baixa = (DownloadTask) new DownloadTask().execute("http://192.168.202.108/json");
+					 person = baixa.getPersona();
+					 //Log.i("missatge", person.getPerson_givenName());
+				}
+			});
+			
+		}
+
+
+		
+		public class DownloadTask extends AsyncTask<String, Long, String> {
+			Person persona;  
+			protected String doInBackground(String... urls) {
+			  
+			    	String string = null;
+			    	string =  HttpRequest.get(urls[0]).body();
+			    	
+			    	Gson parser = new Gson();
+			    	persona = parser.fromJson(string, Person.class);
+		
+			      return string;
+			   
+			  }
+
+			  public Person getPersona() {
+				return persona;
+			}
+			  public String getPersonaName() {
+				return persona.person_givenName;
+			}
+
+			protected void onProgressUpdate(Long... progress) {
+			    Log.d("MyApp", "Downloaded bytes: " + progress[0]);
+			  }
+
+			  protected void onPostExecute(String string) {
+			   
+				  TextView mytext = (TextView) findViewById(R.id.textView2);
+			     // mytext.setText(string);
+			      Log.d("MyApp", "Json de la web: " + persona.person_givenName);
+
+			  }
+			  
+		}
+		
+		
+
+		
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+		
+		
+	}
+
+}
---
+++ src/com/iesebre/DAM2/gson/Person.java
@@ -0,0 +1,199 @@
+package com.iesebre.DAM2.gson;
+
+public class Person {
+	int person_id;
+	String person_givenName;
+	String person_sn1;
+	String person_sn2;
+	String person_email;
+	String person_secondary_email;
+	String person_official_id;
+	int person_official_id_type;
+	String person_date_of_birth;
+	String person_gender;
+	String person_secondary_official_id;
+	String person_homePostalAdress;
+	int person_photo;
+	int person_locality_id;
+	String person_telephoneNumber;
+	String person_mobile;
+	int bank_account_id;
+	String person_notes;
+	String person_entryDate;
+	String person_last_update;
+	int person_creationUserId;
+	int person_lastupdateUserId;
+	String person_markedForDeletion;
+	String person_markedForDeletionDate;
+	String date_of_birth;
+
+
+public Person(int person_id, String person_givenName, String person_sn1,
+			String person_sn2) {
+		super();
+		this.person_id = person_id;
+		this.person_givenName = person_givenName;
+		this.person_sn1 = person_sn1;
+		this.person_sn2 = person_sn2;
+	}
+public Person(int i, String string, String string2, String string3,
+		String string4, String string5, String string6, int j, String string7,
+		String string8, String string9, String string10, int k, int l,
+		String string11, String string12, int m, String string13,
+		String string14, String string15, int n, int o, String string16,
+		String string17, String string18) {
+	// TODO Auto-generated constructor stub
+}
+public int getPerson_id() {
+	return person_id;
+}
+public void setPerson_id(int person_id) {
+	this.person_id = person_id;
+}
+public String getPerson_givenName() {
+	return person_givenName;
+}
+public void setPerson_givenName(String person_givenName) {
+	this.person_givenName = person_givenName;
+}
+public String getPerson_sn1() {
+	return person_sn1;
+}
+public void setPerson_sn1(String person_sn1) {
+	this.person_sn1 = person_sn1;
+}
+public String getPerson_sn2() {
+	return person_sn2;
+}
+public void setPerson_sn2(String person_sn2) {
+	this.person_sn2 = person_sn2;
+}
+public String getPerson_email() {
+	return person_email;
+}
+public void setPerson_email(String person_email) {
+	this.person_email = person_email;
+}
+public String getPerson_secondary_email() {
+	return person_secondary_email;
+}
+public void setPerson_secondary_email(String person_secondary_email) {
+	this.person_secondary_email = person_secondary_email;
+}
+public String getPerson_official_id() {
+	return person_official_id;
+}
+public void setPerson_official_id(String person_official_id) {
+	this.person_official_id = person_official_id;
+}
+public int getPerson_official_id_type() {
+	return person_official_id_type;
+}
+public void setPerson_official_id_type(int person_official_id_type) {
+	this.person_official_id_type = person_official_id_type;
+}
+public String getPerson_date_of_birth() {
+	return person_date_of_birth;
+}
+public void setPerson_date_of_birth(String person_date_of_birth) {
+	this.person_date_of_birth = person_date_of_birth;
+}
+public String getPerson_gender() {
+	return person_gender;
+}
+public void setPerson_gender(String person_gender) {
+	this.person_gender = person_gender;
+}
+public String getPerson_secondary_official_id() {
+	return person_secondary_official_id;
+}
+public void setPerson_secondary_official_id(String person_secondary_official_id) {
+	this.person_secondary_official_id = person_secondary_official_id;
+}
+public String getPerson_homePostalAdress() {
+	return person_homePostalAdress;
+}
+public void setPerson_homePostalAdress(String person_homePostalAdress) {
+	this.person_homePostalAdress = person_homePostalAdress;
+}
+public int getPerson_photo() {
+	return person_photo;
+}
+public void setPerson_photo(int person_photo) {
+	this.person_photo = person_photo;
+}
+public int getPerson_locality_id() {
+	return person_locality_id;
+}
+public void setPerson_locality_id(int person_locality_id) {
+	this.person_locality_id = person_locality_id;
+}
+public String getPerson_telephoneNumber() {
+	return person_telephoneNumber;
+}
+public void setPerson_telephoneNumber(String person_telephoneNumber) {
+	this.person_telephoneNumber = person_telephoneNumber;
+}
+public String getPerson_mobile() {
+	return person_mobile;
+}
+public void setPerson_mobile(String person_mobile) {
+	this.person_mobile = person_mobile;
+}
+public int getBank_account_id() {
+	return bank_account_id;
+}
+public void setBank_account_id(int bank_account_id) {
+	this.bank_account_id = bank_account_id;
+}
+public String getPerson_notes() {
+	return person_notes;
+}
+public void setPerson_notes(String person_notes) {
+	this.person_notes = person_notes;
+}
+public String getPerson_entryDate() {
+	return person_entryDate;
+}
+public void setPerson_entryDate(String person_entryDate) {
+	this.person_entryDate = person_entryDate;
+}
+public String getPerson_last_update() {
+	return person_last_update;
+}
+public void setPerson_last_update(String person_last_update) {
+	this.person_last_update = person_last_update;
+}
+public int getPerson_creationUserId() {
+	return person_creationUserId;
+}
+public void setPerson_creationUserId(int person_creationUserId) {
+	this.person_creationUserId = person_creationUserId;
+}
+public int getPerson_lastupdateUserId() {
+	return person_lastupdateUserId;
+}
+public void setPerson_lastupdateUserId(int person_lastupdateUserId) {
+	this.person_lastupdateUserId = person_lastupdateUserId;
+}
+public String getPerson_markedForDeletion() {
+	return person_markedForDeletion;
+}
+public void setPerson_markedForDeletion(String person_markedForDeletion) {
+	this.person_markedForDeletion = person_markedForDeletion;
+}
+public String getPerson_markedForDeletionDate() {
+	return person_markedForDeletionDate;
+}
+public void setPerson_markedForDeletionDate(String person_markedForDeletionDate) {
+	this.person_markedForDeletionDate = person_markedForDeletionDate;
+}
+public String getDate_of_birth() {
+	return date_of_birth;
+}
+public void setDate_of_birth(String date_of_birth) {
+	this.date_of_birth = date_of_birth;
+}
+}
+
+
